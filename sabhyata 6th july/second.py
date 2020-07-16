41485e726a3b2613b8168da53ebcfd4cd6f3d3aa
sabhyatabadkelwar <68236414+sabhyatabadkelwar@users.noreply.github.com>
2020-07-16 11:41:31 +0530

    Add files via upload

---
+++ sabhyata 6th july/second.py
@@ -0,0 +1,7 @@
+number1 = int(input("please enter the first number"))
+number2 = int(input("please enter the second number"))
+if number1>number2:
+    print(f"subtraction of two number{number1-number2}is")
+else:
+    print(f"subtraction of two number{number2-number1}is")
+
