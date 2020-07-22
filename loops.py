4c76664e7a4c23158d11df42a036a9ca24176fc9
sabhyatabadkelwar <68236414+sabhyatabadkelwar@users.noreply.github.com>
2020-07-22 15:06:01 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,9 @@
+number = 50616
+count = 0
+while number<151515100:
+    print("my name is sabhyata")
+    number+=15194
+    count+=1
+
+print(f"my loop was executed {count} upto this much time", number)
+
